8e827c68486fbe5bbc13804bb0a1a8fa5e468149
WumaCoder <1829913225@qq.com>
2021-03-31 14:05:16 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import alias from "@rollup/plugin-alias";
 import { babel } from "@rollup/plugin-babel";
 
 export default {
@@ -6,7 +7,11 @@ export default {
     file: "./lib/node-oj-stdin.js",
     format: "cjs",
   },
+  external: ["readline"],
   plugins: [
+    alias({
+      entries: [{ find: /^src\/(.*)/, replacement: __dirname + "/src/$1" }],
+    }),
     babel({
       babelHelpers: "bundled",
     }),
